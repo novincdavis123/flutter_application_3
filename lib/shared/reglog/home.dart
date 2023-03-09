64052eab3c920d40871e0e3cb87bb47d98877f40
novincdavis123 <novindavisks@gmail.com>
2023-03-09 23:08:54 +0530

    commit

---
+++ lib/shared/reglog/home.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_3/shared/reglog/log.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+
+class Home extends StatefulWidget{
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  late SharedPreferences logindata;
+  late String name;
+
+  @override
+  void initState() {
+    initial();
+  }
+
+  void initial() async {
+    logindata = await SharedPreferences.getInstance();
+    setState(() {
+      name = logindata.getString('name')!;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Shared Preference Example"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(26.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Center(
+              child: Text(
+                'Hai $name Welcome To Luminar ',
+                style: const TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                logindata.setBool('newuser', true);
+                Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: (context) => LoginPage()));
+              },
+              child: const Text('LogOut'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
