436029b2e83e124d3385e9179305326447b27f90
novincdavis123 <novindavisks@gmail.com>
2023-03-16 11:50:22 +0530

    commit

--- lib/hotelnew/hotel2.dart
+++ lib/hotelnew/hotel2.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_snake_navigationbar/flutter_snake_navigationbar.dart';
 
@@ -7,6 +8,23 @@ class Hotel2 extends StatefulWidget {
 }
 
 class _Hotel2State extends State<Hotel2> {
+  var list1 = [
+    
+            'https://images.unsplash.com/photo-1618773928121-c32242e63f39?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8aG90ZWx8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+    
+            'https://images.unsplash.com/photo-1444201983204-c43cbd584d93?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGhvdGVsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    
+            'https://images.unsplash.com/photo-1611892440504-42a792e24d32?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGhvdGVsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    
+            'https://images.unsplash.com/photo-1512918728675-ed5a9ecdebfd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjd8fGhvdGVsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+  ];
+  var list2 = ['Crown Plaza', 'Hotel Merriot', 'HolyDayln', 'Marriot'];
+  var list3 = [
+    'A 5 star Hotel in...',
+    'A 4 star Hotel in...',
+    'A 3 star Hotel in...',
+    'A 5 star Hotel in...'
+  ];
   var snakeShape;
   var selectedColor;
   var showUnselectedLabels = true;
@@ -63,6 +81,86 @@ class _Hotel2State extends State<Hotel2> {
             padding: const EdgeInsets.all(8.0),
             child: Text('Popular Hotel',
                 style: TextStyle(fontWeight: FontWeight.bold)),
+          ),
+          CarouselSlider(
+        items: List.generate(4, (index) {
+          return Card(
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+              child: Column(crossAxisAlignment: CrossAxisAlignment.start,mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+            Container(height: 150,decoration:BoxDecoration(image: DecorationImage(fit: BoxFit.cover,image:NetworkImage(list1[index])))),
+            Text(
+              list2[index],
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Text(list3[index]),
+            Row(
+              children: [
+                Text('\$180/night',style: TextStyle(color: Colors.blue)),
+                SizedBox(width: 60),
+                Text('4.5',style: TextStyle(color: Colors.blue)),
+                Icon(Icons.star,color: Colors.blue)
+              ],
+            ),
+          ]));
+        }),
+        options: CarouselOptions(
+          height: 230,
+          pauseAutoPlayInFiniteScroll: true,
+          autoPlay: false,
+          viewportFraction: .4,
+          aspectRatio: 16 / 7,
+          initialPage: 2,
+        ),
+      ),
+          Container(
+            child: ListTile(
+              leading: Text('Hotel Packages',
+                  style: TextStyle(fontWeight: FontWeight.bold)),
+              trailing: Text('View All', style: TextStyle(color: Colors.blue)),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: 4,
+                itemBuilder: ((context, index) {
+                  return Card(
+                      child: Row(
+              children: [
+              Container(height: 80,width: 100,decoration:BoxDecoration(image: DecorationImage(fit:BoxFit.cover,image: NetworkImage(list1[index]))),),
+              SizedBox(height: 60,width: 70),
+              Column(children: [
+                Text(list2[index],style: TextStyle(fontWeight: FontWeight.bold)),
+                Text(list3[index]),
+                Text('\$180/night',style: TextStyle(color: Colors.blue)),
+                Row(
+                  children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Icon(Icons.car_crash,color: Colors.blue),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Icon(Icons.headphones_battery_outlined,color: Colors.blue),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Icon(Icons.no_drinks_outlined,color: Colors.blue),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Icon(Icons.wifi,color: Colors.blue),
+                  ),
+                ],),
+              ],),
+              ElevatedButton(onPressed: (){}, child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('Book'),
+              ))
+            ],),);
+                })),
           ),
         ],
       ),
